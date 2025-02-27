dcdedc7a119f9411a4fa88b4272f0548d4163ed5
jihyun-00 <rkdwl811@naver.com>
2025-02-27 17:16:07 +0900

    컬렉션 학생정보

--- 09_inheritance_Homework_jihyun/src/com/hw2/run/Run.java
+++ 09_inheritance_Homework_jihyun/src/com/hw2/run/Run.java
@@ -2,6 +2,7 @@ package com.hw2.run;
 
 import com.hw2.model.dto.Employee;
 import com.hw2.model.dto.Person;
+import com.hw2.model.dto.Prisoner;
 import com.hw2.model.service.Company;
 import com.hw2.model.service.ManagementSystem;
 import com.hw2.model.service.Prison;
@@ -10,31 +11,42 @@ public class Run {
 
 	public static void main(String[] args) {
 
-		//company 생성자를 이용하여 10칸짜리 Employee 객체배열 생성하는
-		//company 객체를 생성하여 managementSystem 자료형의 company 주소 저장
-		Company com = new Company(10);
-		ManagementSystem Company = com;
+		ManagementSystem company = new Company(10);
+		ManagementSystem prison = new Prison(10);
+		// 부모타입 참조변수 = 자식객체 (업캐스팅)
+		
+		// 직원 새로 등록하기 2명
+		company.addPerson(new Employee("EMP001", "유재석", "기획팀")); // 다형성 업캐스팅 사용
+		company.addPerson(new Employee("EMP002", "정형돈", "개발팀")); // 다형성 업캐스팅 사용
+		
+		// 등록된 직원 모두 조회
+		company.displayAllPerson(); // 동적바인딩
+		
+		System.out.println("===========================================");
+		
+		// 수감자 새로 등록하기 2명
+		prison.addPerson(new Prisoner("1205", "정준하", "밥도둑"));
+		prison.addPerson(new Prisoner("0705", "박명수", "웃음연쇄살인"));
+		
+		// 등록된 수감자 모두 조회
+		prison.displayAllPerson();
+		
+		System.out.println("===========================================");
+		
+		// EMP001 id를 가진 직원 삭제
+		company.removePerson("EMP001");
+		
+		// 1205 id를 가진 수감자 삭제
+		prison.removePerson("1205");
+		
+		System.out.println("===========================================");
+		
+		// 현재 남은 직원 모두 조회
+		company.displayAllPerson(); // 동적바인딩
+		// 현재 남은 수감자 모두 조회
+		prison.displayAllPerson(); // 동적바인딩
 		
 		
-		//prison 생성자를 이용하여 10칸짜리 Employee 객체배열 생성하는
-		//prison 객체를 생성하여 managementSystem 자료형의 prison 주소 저장
-		Prison pri = new Prison(10);
-		ManagementSystem Prison = pri;
-		
-		
-		//2명 정도 직원 새로 등록
-		Person[] person = new Employee[2];
-		person[0] = new Employee("EMP001", "유재석", "기획팀");
-		person[1] = new Employee("EMP002", "정형돈", "개발팀");
-		
-		Company.addPerson(person[0]);
-		Company.addPerson(person[1]);
-		
-		//등록한 직원 전체 조회
-		Company.displayAllPerson();
-		
-		//2명 정도 수감자 새로 등록
-		//등록한 수감자 전체 조회
 		
 		
 	}
