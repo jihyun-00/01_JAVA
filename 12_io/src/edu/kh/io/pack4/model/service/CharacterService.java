6eb6ab2fecb10aead4c26e4e98f3a0abb48e7c07
jihyun-00 <rkdwl811@naver.com>
2025-03-06 17:53:46 +0900

    네트워크 완료

---
+++ 12_io/src/edu/kh/io/pack4/model/service/CharacterService.java
@@ -0,0 +1,118 @@
+package edu.kh.io.pack4.model.service;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CharacterService {
+
+	/*
+	 * 문자 기반 스틂 - 2byte 문자 단위로 입/출력하는 스트림
+	 * 
+	 * -Reader(입력), Writer(출력) 최상위 클래스
+	 * 
+	 * - 문자만 작성된 파일, 채팅, 인터넷, 요청 시 데이터 전달
+	 * 
+	 */
+
+	/**
+	 * 문자 기반 파일 입력(파일 읽어오기) + 보조 스트림Buffered 사용
+	 */
+	public void fileInput() {
+
+		// 스트림 참조 변수 선언
+		FileReader fr = null; // 문자 기반 파일 입력 스트림
+		BufferedReader br = null; // 문자 기반 보조 스트림
+
+		try {
+
+			// 문자 기반 스트림 생성하기
+			fr = new FileReader("/io_test/20250305/노래가사.txt");
+
+			// 보조 스트림 생성
+			br = new BufferedReader(fr);
+
+			String line = null; // 한줄을 읽어와 저장할 변수
+
+			while (true) {
+
+				// BufferedReader.readLine() : 한줄씩 문자열을 읽는 역할(줄단위)
+				// ->개행문자가 나올 때까지의 문자열을 읽음
+				line = br.readLine();
+
+				if (line == null) { // 더이상 읽어올 내용이 없을 때
+
+					break;
+
+				}
+
+				System.out.println(line);
+
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	/**
+	 * 문자 기반 파일 출력(파일 내보내기) +보조 스트림 Buffered 사용
+	 */
+	public void fileOutput() {
+
+		// 스트림 참조변수 선언
+		FileWriter fw = null; // 문자 기반 파일 출력 스트림
+		BufferedWriter dw = null; // 문자 출력 보고 스트림
+
+		try {
+			
+			
+			StringBuilder sb = new StringBuilder();
+			
+			sb.append("오늘은 3월 6일 목요일입니다\n");
+			sb.append("벌써 목요일이라니, 기분이 좋네요\n");
+			sb.append("다들 힘내서 공부합시다!\n");
+			sb.append("그리고 오늘 쪽지시험 있어요^^\n");
+			
+			String content = sb.toString();
+			
+			//content에 저장된 문자열을
+			// /io_test/20250305/출력테스트.txt 파일로 출력하기
+			
+			//파일과 출력스트림 연결(파일이 없으면 생성해줌)
+			fw = new FileWriter("/io_test/20250305/출력테스트.txt");
+			
+			dw = new BufferedWriter(fw);
+			
+			dw.write(content);
+			
+			//bw.flush()
+			
+			System.out.println("출력 완료");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (dw != null)
+					dw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
